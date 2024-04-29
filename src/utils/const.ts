8f36eab760183f1089b83fad62e6da133092205c
Zoom <egor147536987@gmail.com>
2024-04-29 18:53:43 +0300

    Change base URLs to production server on Render
    From:
    ```javascript
    export const BASE_URL = 'http://localhost:5000'
    export const BASE_URL_CDN = 'http://localhost

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000'
-export const BASE_URL_CDN = 'http://localhost:5000'
+export const BASE_URL = 'https://nest-ecommerse.onrender.com'
+export const BASE_URL_CDN = 'https://nest-ecommerse.onrender.com'
